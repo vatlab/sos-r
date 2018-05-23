facfd5f8a2c4815e4aa5855cbf7bb42b8cb79e8c
Man Chong Leong <ml69@rice.edu>
2018-05-23 16:56:51 -0500

    fix the problem of invalid key name from SoS dict to R list #6

--- src/sos_r/kernel.py
+++ src/sos_r/kernel.py
@@ -63,7 +63,17 @@ def _R_repr(obj):
     elif obj is None:
         return 'NULL'
     elif isinstance(obj, dict):
-        return 'list(' + ','.join('{}={}'.format(x, _R_repr(y)) for x,y in obj.items()) + ')'
+        # check whether each key string is a valid string in R
+        import re
+        _sos_dealing_with_invalid_key_pattern = re.compile("\W|^(?=\d)")
+        def _sos_dealing_with_invalid_key_make_name(input_string):
+            name = str(input_string)
+            if name.isalpha():
+                return name
+            if not name[0].isalpha():
+                name = 'X' + name
+                return re.sub(_sos_dealing_with_invalid_key_pattern, '_', name)
+        return 'list(' + ','.join('{}={}'.format(_sos_dealing_with_invalid_key_make_name(str(x)), _R_repr(y)) for x,y in obj.items()) + ')'
     elif isinstance(obj, set):
         return 'list(' + ','.join(_R_repr(x) for x in obj) + ')'
     else:
