8ec1ffd70414966141254c5098b0d025a76822fc
Bo Peng <ben.bog@gmail.com>
2019-04-25 01:30:12 -0500

    Rewrite tests

--- test/test_data_exchange.py
+++ test/test_data_exchange.py
@@ -3,195 +3,175 @@
 # Copyright (c) Bo Peng and the University of Texas MD Anderson Cancer Center
 # Distributed under the terms of the 3-clause BSD License.
 
-
-import pytest
-import os
-import sys
-import tempfile
-from textwrap import dedent
 from sos_notebook.test_utils import NotebookTest
+import random
 
-class TestInterface(NotebookTest):
+class TestDataExchange(NotebookTest):
+    def _var_name(self):
+        if not hasattr(self, '_var_idx'):
+            self._var_idx = 0
+        self._var_idx += 1
+        return f'var{self._var_idx}'
+
+    def get_from_SoS(self, notebook, sos_expr):
+        var_name = self._var_name()
+        notebook.call(f'{var_name} = {sos_expr}', kernel='SoS')
+        return notebook.check_output(f'''\
+            %get {var_name}
+            {var_name}''', kernel='R')
+
+    def put_to_SoS(self, notebook, r_expr):
+        var_name = self._var_name()
+        notebook.call(f'''\
+            %put {var_name}
+            {var_name} <- {r_expr}
+            ''',
+                      kernel='R')
+        return notebook.check_output(f'print(repr({var_name}))', kernel='SoS')
+
+    def test_get_none(self, notebook):
+        assert 'NULL' == self.get_from_SoS(notebook, 'None')
+
+    def test_put_null(self, notebook):
+        assert 'None' == self.put_to_SoS(notebook, 'NULL')
+
+    def test_get_int(self, notebook):
+        assert '123' == self.get_from_SoS(notebook, '123')
+        assert '1234567891234' == self.get_from_SoS(
+            notebook, '1234567891234')
+        # longer int does not gurantee precision any more, I am not sure
+        # if the following observation will be consistent (784 vs 789)
+        assert '123456789123456784' == self.get_from_SoS(
+            notebook, '123456789123456789')
 
     def test_put_int(self, notebook):
-        idx = notebook.call_in_kernel(content=dedent('''\
-            %put var1 var2
-            var1 = 100
-            var2 = 123456789123456789
-            '''), kernel="R")
-        idx = notebook.call_in_kernel(content=dedent('''\
-            var1
-            '''), kernel="SoS")
-        assert '100' == notebook.get_cell_output(index=idx)        
-        idx = notebook.call_in_kernel(content=dedent('''\
-            var2
-            '''), kernel="SoS")
-        assert '123456789123456789' == notebook.get_cell_output(index=idx)
+        assert '123' == self.put_to_SoS(notebook, '123')
+        assert '1234567891234' == self.put_to_SoS(
+            notebook, '1234567891234')
+        # longer int does not gurantee precision any more, I am not sure
+        # if the following observation will be consistent (784 vs 789)
+        assert '123456789123456784' == self.put_to_SoS(
+            notebook, '123456789123456789')
 
-    def testGetPythonDataFrameFromR(self):
-        # Python -> R
-        with sos_kernel() as kc:
-            iopub = kc.iopub_channel
-            # create a data frame
-            execute(kc=kc, code='''
-import pandas as pd
-import numpy as np
-arr = np.random.randn(1000)
-arr[::10] = np.nan
-df = pd.DataFrame({'column_{0}'.format(i): arr for i in range(10)})
-''')
-            clear_channels(iopub)
-            execute(kc=kc, code="%use R")
-            wait_for_idle(kc)
-            execute(kc=kc, code="%get df")
-            wait_for_idle(kc)
-            execute(kc=kc, code="dim(df)")
-            res = get_display_data(iopub)
-            self.assertEqual(res, '[1] 1000   10')
-            execute(kc=kc, code="%use sos")
-            wait_for_idle(kc)
-            #
+    def test_get_double(self, notebook):
+        # FIXME: can we improve the precision here? Passing float as string
+        # is certainly a bad idea.
+        val = str(random.random())
+        assert abs(float(val) - float(self.get_from_SoS(notebook, val))) < 1e-10
 
-    def testGetPythonDictWithSpecialKeyFromR(self):
-        # Python -> R
-        with sos_kernel() as kc:
-            iopub = kc.iopub_channel
-            # create a data frame
-            execute(kc=kc, code='''
-special_dict = {}
-special_dict['11111'] = 1
-special_dict['_1111'] = 'a'
-special_dict[11112] = 2
-special_dict[(1,2)] = 3
-''')
-            wait_for_idle(kc)
-            execute(kc=kc, code='''%use R
-%get special_dict
-keys = names(special_dict)
-''')
-            wait_for_idle(kc)
-            execute(kc=kc, code='''%use sos
-%get keys --from R            
-%dict keys
-''')
-            res = get_result(iopub)['keys']
-            for key in ["X11111",  "X_1111",  "X11112",  "X_1__2_"]:
-                self.assertTrue(key in res, f"Expecting {key}")
+    def test_put_double(self, notebook):
+        val = str(random.random())
+        assert abs(float(val) - float(self.put_to_SoS(notebook, val))) < 1e-10
 
-    def testGetPythonDataFromR(self):
-        with sos_kernel() as kc:
-            iopub = kc.iopub_channel
-            execute(kc=kc, code='''
-null_var = None
-num_var = 123
-import numpy
-import pandas
-num_arr_var = numpy.array([1, 2, 3])
-logic_var = True
-logic_arr_var = [True, False, True]
-char_var = '1"23'
-char_arr_var = ['1', '2', '3']
-list_var = [1, 2, '3']
-dict_var = dict(a=1, b=2, c='3')
-set_var = {1, 2, '3'}
-mat_var = numpy.matrix([[1,2],[3,4]])
-recursive_var = {'a': {'b': 123}, 'c': True}
-comp_var = 1+2j
-seri_var = pandas.Series([1,2,3,3,3,3])
-''')
-            wait_for_idle(kc)
-            execute(kc=kc, code='''\
-%use R
-%get null_var num_var num_arr_var logic_var logic_arr_var char_var char_arr_var mat_var set_var list_var dict_var recursive_var comp_var seri_var
-%dict -r
-%put null_var num_var num_arr_var logic_var logic_arr_var char_var char_arr_var mat_var set_var list_var dict_var recursive_var comp_var seri_var
-%use sos
-seri_var = list(seri_var)
-''')
-            wait_for_idle(kc)
-            execute(kc=kc, code='''\
-%dict null_var num_var num_arr_var logic_var logic_arr_var char_var char_arr_var mat_var set_var list_var dict_var recursive_var comp_var seri_var
-''')
-            res = get_result(iopub)
-            self.assertEqual(res['null_var'], None)
-            self.assertEqual(res['num_var'], 123)
-            self.assertEqual(res['num_arr_var'].tolist(), [1, 2, 3])
-            self.assertEqual(res['logic_var'], True)
-            self.assertEqual(res['logic_arr_var'], [True, False, True])
-            self.assertEqual(res['char_var'], '1"23')
-            self.assertEqual(res['char_arr_var'], ['1', '2', '3'])
-            self.assertEqual(res['list_var'], [1,2,'3'])
-            self.assertEqual(res['dict_var'], {'a': 1, 'b': 2, 'c': '3'})
-            self.assertEqual(res['mat_var'].shape, (2,2))
-            self.assertEqual(res['recursive_var'],  {'a': {'b': 123}, 'c': True})
-            self.assertEqual(res['comp_var'], 1+2j)
-            self.assertEqual(res['seri_var'], [1,2,3,3,3,3])
+    def test_get_logic(self, notebook):
+        assert 'TRUE' == self.get_from_SoS(notebook, 'True')
+        assert 'FALSE' == self.get_from_SoS(notebook, 'False')
 
-    def testPutRDataFrameToPython(self):
-        # R -> Python
-        with sos_kernel() as kc:
-            iopub = kc.iopub_channel
-            # create a data frame
-            execute(kc=kc, code='%use R')
-            wait_for_idle(kc)
-            execute(kc=kc, code="%put mtcars")
-            assemble_output(iopub)
-            # the message can contain "Loading required package feathre"
-            #self.assertEqual(stderr, '')
-            execute(kc=kc, code="%use sos")
-            wait_for_idle(kc)
-            execute(kc=kc, code="mtcars.shape")
-            res = get_result(iopub)
-            self.assertEqual(res, (32, 11))
-            execute(kc=kc, code="mtcars.index[0]")
-            res = get_result(iopub)
-            self.assertEqual(res, 'Mazda RX4')
+    def test_put_logic(self, notebook):
+        assert 'True' == self.put_to_SoS(notebook, 'TRUE')
+        assert 'False' == self.put_to_SoS(notebook, 'FALSE')
 
-    def testPutRDataToPython(self):
-        with sos_kernel() as kc:
-            iopub = kc.iopub_channel
-            # create a data frame
-            execute(kc=kc, code="""\
-%use R
-null_var = NULL
-num_var = 123
-num_arr_var = c(1, 2, 3)
-logic_var = TRUE
-logic_arr_var = c(TRUE, FALSE, TRUE)
-char_var = '1\"23'
-char_arr_var = c(1, 2, '3')
-list_var = list(1, 2, '3')
-named_list_var = list(a=1, b=2, c='3')
-mat_var = matrix(c(1,2,3,4), nrow=2)
-recursive_var = list(a=1, b=list(c=3, d='whatever'))
-comp_var = 1+2i
-seri_var = setNames(c(1,2,3,3,3,3),c(0:5))
-""")
-            wait_for_idle(kc)
-            execute(kc=kc, code='''\
-%put null_var num_var num_arr_var logic_var logic_arr_var char_var char_arr_var mat_var list_var named_list_var recursive_var comp_var seri_var
-%use sos
-seri_var = list(seri_var)
-''')
-            wait_for_idle(kc)
-            execute(kc=kc, code="%dict null_var num_var num_arr_var logic_var logic_arr_var char_var char_arr_var mat_var list_var named_list_var recursive_var comp_var seri_var")
-            res = get_result(iopub)
-            self.assertEqual(res['null_var'], None)
-            self.assertEqual(res['num_var'], 123)
-            self.assertEqual(res['num_arr_var'], [1,2,3])
-            self.assertEqual(res['logic_var'], True)
-            self.assertEqual(res['logic_arr_var'], [True, False, True])
-            self.assertEqual(res['char_var'], '1"23')
-            self.assertEqual(res['char_arr_var'], ['1', '2', '3'])
-            self.assertEqual(res['list_var'], [1,2,'3'])
-            self.assertEqual(res['named_list_var'], {'a': 1, 'b': 2, 'c': '3'})
-            self.assertEqual(res['mat_var'].shape, (2,2))
-            self.assertEqual(res['recursive_var'], {'a': 1, 'b': {'c': 3, 'd': 'whatever'}})
-            self.assertEqual(res['comp_var'], 1+2j)
-            self.assertEqual(res['seri_var'], [1,2,3,3,3,3])
-            execute(kc=kc, code="%use sos")
-            wait_for_idle(kc)
+    def test_get_num_array(self, notebook):
+        assert '1' == self.get_from_SoS(notebook, '[1]')
+        assert '1 2' == self.get_from_SoS(notebook, '[1, 2]')
+        #
+        assert '1.23' == self.get_from_SoS(notebook, '[1.23]')
+        assert '1.4 2' == self.get_from_SoS(notebook, '[1.4, 2]')
 
+    def test_put_num_array(self, notebook):
+        # Note that single element numeric array is treated as single value
+        assert '1' == self.put_to_SoS(notebook, 'c(1)')
+        assert '[1, 2]' == self.put_to_SoS(notebook, 'c(1, 2)')
+        #
+        assert '1.23' == self.put_to_SoS(notebook, 'c(1.23)')
+        assert '[1.4, 2]' == self.put_to_SoS(notebook, 'c(1.4, 2)')
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_get_logic_array(self, notebook):
+        assert 'TRUE' == self.get_from_SoS(notebook, '[True]')
+        assert 'TRUE FALSE TRUE' == self.get_from_SoS(notebook, '[True, False, True]')
+
+    def test_put_logic_array(self, notebook):
+        # Note that single element numeric array is treated as single value
+        assert 'True' == self.put_to_SoS(notebook, 'c(TRUE)')
+        assert '[True, False, True]' == self.put_to_SoS(notebook, 'c(TRUE, FALSE, TRUE)')
+
+    def test_get_str(self, notebook):
+        assert "'ab c d'" == self.get_from_SoS(notebook, "'ab c d'")
+        assert "'ab\\td'" == self.get_from_SoS(notebook, r"'ab\td'")
+
+    def test_put_str(self, notebook):
+        assert "'ab c d'" == self.put_to_SoS(notebook, "'ab c d'")
+        assert "'ab\\td'" == self.put_to_SoS(notebook, r"'ab\td'")
+
+    def test_get_mixed_list(self, notebook):
+        assert "1.4\nTRUE\n'asd'" == self.get_from_SoS(notebook, '[1.4, True, "asd"]')
+
+    def test_put_mixed_list(self, notebook):
+        # R does not have mixed list, it just convert everything to string.
+        assert "['1.4', 'TRUE', 'asd']" == self.put_to_SoS(notebook, 'c(1.4, TRUE, "asd")')
+
+    def test_get_dict(self, notebook):
+        # Python does not have named ordered list, so get dictionary
+        assert "$a\n1\n$b\n2\n$c\n'3'" == self.get_from_SoS(notebook, "dict(a=1, b=2, c='3')")
+
+    def test_put_named_list(self, notebook):
+        assert "{'a': 1, 'b': 2, 'c': '3'}" == self.put_to_SoS(notebook, "list(a=1, b=2, c='3')")
+
+    def test_get_set(self, notebook):
+        output = self.get_from_SoS(notebook, "{1.5, 'abc'}")
+        assert "1.5\n'abc'" == output or "'abc'\n1.5" == output
+
+    def test_put_unnamed_list(self, notebook):
+        output = self.put_to_SoS(notebook, "list(1.5, 'abc')")
+        assert "[1.5, 'abc']" == output or "['abc', 1.5]" == output
+
+    def test_get_complex(self, notebook):
+        assert "1+2.2i" == self.get_from_SoS(notebook, "complex(1, 2.2)")
+
+    def test_put_complex(self, notebook):
+        assert "(1+2.2j)" == self.put_to_SoS(notebook, "complex(real=1, imaginary=2.2)")
+
+    def test_get_recursive(self, notebook):
+        assert "$a\n1\n$b\n$c\n3\n$d\n'whatever'" == self.get_from_SoS(notebook, "{'a': 1, 'b': {'c': 3, 'd': 'whatever'}}")
+
+    def test_put_recursive(self, notebook):
+        assert "{'a': 1, 'b': {'c': 3, 'd': 'whatever'}}" == self.put_to_SoS(notebook, "list(a=1, b=list(c=3, d='whatever'))")
+
+    def test_get_series(self, notebook):
+        notebook.call('import pandas as pd', kernel='SoS')
+        assert "0\n5\n1\n6\n2\n7" == self.get_from_SoS(notebook, 'pd.Series([5 ,6, 7])')
+
+    def test_put_series(self, notebook):
+        output = self.put_to_SoS(notebook, "setNames(c(11, 22, 33), c('a', 'b', 'c'))")
+        assert 'a    11' in output and 'b    22' in output and 'c    33' in output
+
+    def test_get_matrix(self, notebook):
+        notebook.call('import numpy as np', kernel='SoS')
+        assert "0 1\n1 2\n3 4" == self.get_from_SoS(notebook, 'np.matrix([[1,2],[3,4]])')
+
+    def test_put_matrix(self, notebook):
+        output = self.put_to_SoS(notebook, "matrix(c(2, 4, 3, 1, 5, 7), nrow=2)")
+        assert 'array' in output and '[2., 3., 5.]' in output and '[4., 1., 7.]' in output
+
+    def test_get_dataframe(self, notebook):
+        notebook.call('''\
+            %put df --to R
+            import pandas as pd
+            import numpy as np
+            arr = np.random.randn(1000)
+            arr[::10] = np.nan
+            df = pd.DataFrame({'column_{0}'.format(i): arr for i in range(10)})
+            ''', kernel='SoS')
+        assert '1000' == notebook.check_output('dim(df)[1]', kernel='R')
+        assert '10' == notebook.check_output('dim(df)[2]', kernel='R')
+
+    def test_put_dataframe(self, notebook):
+        notebook.call('%put mtcars', kernel='R')
+        assert '32' == notebook.check_output('mtcars.shape[0]', kernel='SoS')
+        assert '11' == notebook.check_output('mtcars.shape[1]', kernel='SoS')
+        assert "'Mazda RX4'" == notebook.check_output('mtcars.index[0]', kernel='SoS')
+
+    def test_get_dict_with_special_keys(self, notebook):
+        output = self.get_from_SoS(notebook, "{'11111': 1, '_1111': 'a', 11112: 2, (1,2): 3}")
+        assert '$X11111' in output and '$X_1111' in output and '$X11112' in output and '$X_1__2_' in output
+
